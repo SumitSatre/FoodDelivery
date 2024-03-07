4a46fa8d5361682f5d13dc0e77e1dd9de4bf0346
Sumit Satre <sumitsatre03@gmail.com>
2024-03-07 20:46:59 +0530

    Food category api updated

--- backend/routes/FoodCategory.js
+++ backend/routes/FoodCategory.js
@@ -4,6 +4,8 @@ const FoodCategoryModel = require("../models/FoodCategoryModel");
 
 const router = express.Router();
 
+// http://localhost:5000/api/upload/categories
+
 router.post("/upload/categories" , async (req ,res , next) => {
     const categoriesList = req.body.categoriesList;
 
